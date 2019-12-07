ec27a02b728047c447efa03d863ef5cdc8aab815
Ryan Avella <ryanavella@users.noreply.github.com>
2019-12-07 15:41:13 -0800

    modifying terminal drawing so that the screen is only cleared at the beginning, and cells are either toggled on or off

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//! Conway's Game of Life, in Rust.
+
 use std::io::{stdout, Write};
 use std::thread;
 use std::time;
@@ -21,36 +23,45 @@ fn main() -> Result<()> {
     board[board.len() / 2][board.len() / 2 + 1] = 1;
     board[board.len() / 2 + 1][board.len() / 2 + 1] = 1;
 
-    loop {
-        let tick_beg = time::Instant::now();
-
-        stdout.queue(terminal::Clear(terminal::ClearType::All))?;
-        for y in 1..(board.len() - 1) {
-            for x in 1..(board.len() - 1) {
-                if board[x][y] == 1 {
-                    stdout
-                        .queue(cursor::MoveTo(x as u16, y as u16))?
-                        .queue(style::PrintStyledContent("█".white()))?;
-                }
+    stdout.queue(terminal::Clear(terminal::ClearType::All))?;
+    for y in 1..(board.len() - 1) {
+        for x in 1..(board.len() - 1) {
+            if board[x][y] == 1 {
+                stdout
+                    .queue(cursor::MoveTo(x as u16, y as u16))?
+                    .queue(style::PrintStyledContent("█".white()))?;
             }
         }
+    }
 
+    loop {
+        let tick_beg = time::Instant::now();
         stdout.flush()?;
 
         let board_old = board;
-        for i in 1..(board.len() - 1) {
-            for j in 1..(board.len() - 1) {
-                let sum = board_old[i - 1][j - 1]
-                    + board_old[i][j - 1]
-                    + board_old[i + 1][j - 1]
-                    + board_old[i - 1][j]
-                    + board_old[i + 1][j]
-                    + board_old[i - 1][j + 1]
-                    + board_old[i][j + 1]
-                    + board_old[i + 1][j + 1];
-                board[i][j] = if sum == 3 || (sum == 2 && board[i][j] == 1) {
+        for y in 1..(board.len() - 1) {
+            for x in 1..(board.len() - 1) {
+                let sum = board_old[y - 1][x - 1]
+                    + board_old[y][x - 1]
+                    + board_old[y + 1][x - 1]
+                    + board_old[y - 1][x]
+                    + board_old[y + 1][x]
+                    + board_old[y - 1][x + 1]
+                    + board_old[y][x + 1]
+                    + board_old[y + 1][x + 1];
+                board[y][x] = if sum == 3 || (sum == 2 && board[y][x] == 1) {
+                    if board_old[y][x] == 0 {
+                        stdout
+                            .queue(cursor::MoveTo(x as u16, y as u16))?
+                            .queue(style::PrintStyledContent("█".white()))?;
+                    }
                     1
                 } else {
+                    if board_old[y][x] == 1 {
+                        stdout
+                            .queue(cursor::MoveTo(x as u16, y as u16))?
+                            .queue(style::PrintStyledContent(" ".white()))?;
+                    }
                     0
                 };
             }
